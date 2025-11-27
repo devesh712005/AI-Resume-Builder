b43400eeab3a617561a46002ee29a7207d7ef77f
devesh712005 <deveshsingh712005@gmail.com>
2025-11-27 19:19:33 +0530

    feat: add template and color selection

---
+++ client/src/components/ProfessionalSummaryForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfessionalSummaryForm = ({ data, onChange, setResumeData }) => {
+  return <div className="space-y-4"></div>;
+};
+
+export default ProfessionalSummaryForm;
